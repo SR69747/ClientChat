d2519abb587d4d86603e76708b4887beafd4d7a0
Sam Raywood <samuel.raywood@lgim.com>
2017-02-24 15:28:12 +0000

    Addition of look and feel in main class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 import gui.Login;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         Login.launchLoginGui();
     }
 }
