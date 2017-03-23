7d7a832b7d0898e11ba990dac0caf5dffc576b9f
JP69745 <juri.prank@lgim.com>
2017-03-23 13:36:21 +0000

    Implemented .txt file and text drop in text area.

--- src/listeners/DropListener.java
+++ src/listeners/DropListener.java
@@ -93,23 +93,45 @@ public final class DropListener implements DropTargetListener, Serializable {
     public void drop(DropTargetDropEvent dtde) {
         try {
             Transferable t = dtde.getTransferable();
-
-            if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+            if (dtde.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                dtde.acceptDrop(dtde.getDropAction());
+                String s = t.getTransferData(DataFlavor.stringFlavor).toString();
+                Sender.sendMessageToServer(s);
+                dtde.dropComplete(true);
+            } else if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
                 dtde.acceptDrop(dtde.getDropAction());
                 Object o = t.getTransferData(DataFlavor.javaFileListFlavor);
                 if (o != null) {
                     String path = o.toString().substring(1, o.toString().length() - 1);
                     if (path.contains(".jpg") || path.contains(".png")) {
                         Sender.sendImageToServer(path);
+                    } else if (path.contains(".txt")) {
+                        String fileData = readFile(path);
+                        Sender.sendMessageToServer(fileData);
                     } else {
                         Chat.displayMessageInHTML("Error: Unknown File", "red", false);
                     }
                 }
                 dtde.dropComplete(true);
+            } else {
+                dtde.rejectDrop();
             }
         } catch (java.io.IOException | UnsupportedFlavorException e2) {
             System.out.println(e2.getMessage());
         }
+    }
+
+    private static String readFile(String path) {
+        StringBuilder fileText = new StringBuilder(5);
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(path)))) {
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                fileText.append(currentLine).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileText.toString();
     }
 
 }
