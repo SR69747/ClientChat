b5a0564d837de0cbd3267beb6e21195e621803b5
Sam Raywood <samuel.raywood@lgim.com>
2017-04-18 15:58:54 +0100

    Addition of registration feature - panels switching using card panel layout Getters created in order to send registration details to server Methods created to display messages

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,6 +1,9 @@
 package gui;
 
+import listeners.CancelListener;
 import listeners.LoginButtonActionListener;
+import listeners.RegistrationButtonListener;
+import listeners.RegistrationPageButtonListener;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,23 +13,49 @@ public final class Login {
     // Main frame initialisation
     private static JFrame loginFrame = new JFrame("Log in");
     private static JPanel loginPanel = new JPanel();
+    private static JPanel registrationPanel = new JPanel();
 
-    // Component initialisation
-    private static JTextField usernameField = new JTextField(15);
-    private static JPasswordField passwordField = new JPasswordField(15);
+    // Card Layout initialisation
+    private static JPanel cardPanel = new JPanel(new CardLayout());
+    private static CardLayout cardLayout = (CardLayout) cardPanel.getLayout();
+
+
+    // Login Component initialisation
+    private static JTextField usernameField = new JTextField(10);
+    private static JPasswordField passwordField = new JPasswordField(10);
     private static JLabel usernameLabel = new JLabel("Username: ");
-    private static JLabel passwordLabel = new JLabel("Password: ");
-    private static JButton loginButton = new JButton("Enter");
+    private static JLabel passwordLabel = new JLabel("Password:  ");
+    private static JButton loginButton = new JButton("Login");
+    private static JButton registrationButton = new JButton("Register");
     private static JTextArea loginMessage = new JTextArea();
     private static Font messageFont = new Font("Calibri", Font.PLAIN, 18);
 
-    // Panel initialisation
+    // Login Panel initialisation
     private static JPanel messagePanel = new JPanel();
     private static JPanel usernamePanel = new JPanel();
     private static JPanel passwordPanel = new JPanel();
     private static JPanel buttonPanel = new JPanel();
     private static Icon loginMenuLogo = new ImageIcon("src/resources/lgim.png");
     private static JLabel imageLabel = new JLabel();
+
+    // Registration Component initialisation
+    private static JLabel registerMessageLabel = new JLabel("");
+    private static JLabel registerUsernameLabel = new JLabel("Username:  ");
+    private static JLabel registerPasswordLabel = new JLabel("Password:  ");
+    private static JLabel retypePasswordLabel = new JLabel("Re-type Password: ");
+    private static JPasswordField registerPasswordField = new JPasswordField(10);
+    private static JPasswordField registerRetypePasswordField = new JPasswordField(10);
+    private static JTextField registrationUsernameField = new JTextField(10);
+    private static JButton cancelButton = new JButton("Back");
+    private static JButton submitRegisterButton = new JButton("Submit");
+
+    // Registration Panel initialisation
+    private static JPanel registrationMessagePanel = new JPanel();
+    private static JPanel registrationUsernamePanel = new JPanel();
+    private static JPanel registrationPasswordPanel = new JPanel();
+    private static JPanel registrationButtonPanel = new JPanel();
+    private static JPanel registrationRetypePanel = new JPanel();
+
 
     public static void launchLoginGui() {
 
@@ -39,7 +68,37 @@ public final class Login {
         loginMessage.setEditable(false);
         loginMessage.setFont(messageFont);
 
-        // Components are split into panels here
+
+        // Registration Components split into panels here
+        registrationMessagePanel.setBackground(Color.WHITE);
+        registrationMessagePanel.add(registerMessageLabel);
+        registrationUsernamePanel.add(Box.createHorizontalStrut(35));
+        registrationUsernamePanel.setBackground(Color.WHITE);
+        registrationUsernamePanel.add(registerUsernameLabel);
+        registrationUsernamePanel.add(registrationUsernameField);
+        registrationPasswordPanel.add(Box.createHorizontalStrut(37));
+        registrationPasswordPanel.setBackground(Color.WHITE);
+        registrationPasswordPanel.add(registerPasswordLabel);
+        registrationPasswordPanel.add(registerPasswordField);
+        registrationRetypePanel.setBackground(Color.WHITE);
+        registrationRetypePanel.add(retypePasswordLabel);
+        registrationRetypePanel.add(registerRetypePasswordField);
+        registrationButtonPanel.setBackground(Color.WHITE);
+        registrationButtonPanel.add(cancelButton);
+        registrationButtonPanel.add(Box.createHorizontalStrut(25));
+        registrationButtonPanel.add(submitRegisterButton);
+
+        // initialise registration panel and add panels to main panel
+        registrationPanel.setVisible(true);
+        registrationPanel.setLayout(new BoxLayout(registrationPanel, BoxLayout.Y_AXIS));
+        registrationPanel.add(registrationMessagePanel);
+        registrationPanel.add(registrationUsernamePanel);
+        registrationPanel.add(registrationPasswordPanel);
+        registrationPanel.add(registrationRetypePanel);
+        registrationPanel.add(registrationButtonPanel);
+
+
+        // Login Components are split into panels here
         messagePanel.add(loginMessage);
         usernamePanel.add(usernameLabel);
         usernamePanel.add(usernameField);
@@ -48,10 +107,11 @@ public final class Login {
         buttonPanel.add(loginButton);
         imageLabel.setIcon(loginMenuLogo);
         buttonPanel.add(imageLabel);
+        buttonPanel.add(Box.createHorizontalStrut(10));
+        buttonPanel.add(registrationButton);
 
-
-        // Panels are then added to main frame here
-        loginPanel.setBounds(300, 300, 300, 300);
+        // Login Panels are then added to main login panel here
+        loginPanel.setBounds(300, 300, 300, 200);
         loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.Y_AXIS));
         loginPanel.add(messagePanel);
         loginPanel.add(usernamePanel);
@@ -62,16 +122,28 @@ public final class Login {
         passwordPanel.setBackground(Color.WHITE);
         buttonPanel.setBackground(Color.WHITE);
 
+        // add login and registration to the card panel
+        cardPanel.add(loginPanel);
+        cardPanel.add(registrationPanel);
 
-        loginFrame.add(loginPanel, BorderLayout.CENTER);
+        loginFrame.add(cardPanel, BorderLayout.CENTER);
+        loginFrame.setPreferredSize(new Dimension(300, 220));
         loginFrame.pack();
 
         loginButton.addActionListener(new LoginButtonActionListener(usernameField, passwordField));
+
+        // Change panel to registration panel
+        registrationButton.addActionListener(new RegistrationPageButtonListener());
+
+        // Change panel to login panel
+        cancelButton.addActionListener(new CancelListener());
+
+        // Submits registration details
+        submitRegisterButton.addActionListener(new RegistrationButtonListener());
     }
 
     //Work on this !
     public static void disableLoginFrame() {
-        //TODO Need to actually close loginFrame, when we terminate our application.
         loginFrame.setVisible(false);
 //       loginFrame.dispatchEvent(new WindowEvent(loginFrame, WindowEvent.WINDOW_CLOSING));
     }
@@ -95,4 +167,58 @@ public final class Login {
     static String getUsername() {
         return usernameField.getText();
     }
+
+    public static void openRegistrationPage() {
+        resetLoginMessage();
+        cardLayout.next(cardPanel);
+        loginFrame.setTitle("Registration");
+    }
+
+    public static void openLoginPage() {
+        cardLayout.next(cardPanel);
+        loginFrame.setTitle("Login");
+        resetRegistrationPage();
+
+    }
+
+    public static String getRegistrationUsername() {
+        return registrationUsernameField.getText();
+    }
+
+    public static String getRegistrationPassword() {
+        return String.valueOf(registerPasswordField.getPassword());
+    }
+
+    public static String getRegistrationRetypePassword() {
+        return String.valueOf(registerRetypePasswordField.getPassword());
+    }
+
+    public static void setRegisterPasswordMatchErrorMessage() {
+        resetRegistrationPage();
+        registerMessageLabel.setText("Passwords do not match!");
+    }
+
+    public static void setRegisterCompleteMessage() {
+        resetRegistrationPage();
+        openLoginPage();
+        JOptionPane.showMessageDialog(loginFrame, ("Registration Complete!"));
+    }
+
+    public static void setFailedRegistrationMessage() {
+        resetRegistrationPage();
+        JOptionPane.showMessageDialog(loginFrame, ("Registration Failed!"));
+    }
+
+    private static void resetRegistrationPage() {
+        registerMessageLabel.setText("");
+        registrationUsernameField.setText("");
+        registerPasswordField.setText("");
+        registerRetypePasswordField.setText("");
+    }
+
+    private static void resetLoginMessage(){
+        loginMessage.setText("");
+    }
+
+
 }
